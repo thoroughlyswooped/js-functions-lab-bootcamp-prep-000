e2d043aebd4f17a997859671eb0bd0047e580501
Ezra T Huffman <joshooha@gmail.com>
2018-09-15 20:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 function happyHolidayTo(reason, name) {
   return `Happy ${reason}, ${name}!`;
